0d5f77cb9527c31dbd6ab4b29c8a6ed18a6ee6b5
ほし <cgastrogation@gmail.com>
2024-04-02 17:02:43 +0900

    [add]_当社についてセクション作成

--- _src/front-page.php
+++ _src/front-page.php
@@ -31,22 +31,32 @@
       <h2 class="fr-mvText__lead">世界で活躍できるグローバルな人材を育てる</h2><!-- /.fr-mvText__lead -->
     </div><!-- /.fr-mvText -->
   </div>
-  <section class="fr-about" id="about">
-    <div class="l-inner">
-      <div class="p-sectionHead">
+  <section class="fr-about p-section" id="about">
+    <div class="l-inner fr-about__inner">
+      <div class="p-section__head">
         <div class="fr-about__title p-title">
           <h2 class="c-title--eg">ABOUT US</h2><!-- /.p-footerLink__title--eg -->
           <h2 class="c-title--JP">当社について</h2><!-- /.p-footerLink__title--eg -->
         </div><!-- /.fr-about__title -->
-        <div class="p-link">
-          <a href="" class="c-link">View more</a><!-- /.c-link -->
+        <div class="p-link c-link--fill u-show--flex">
+          <a href="<?php echo esc_url(home_url('/about/')); ?>" class="c-link">View more</a><!-- /.c-link -->
           <div class="c-circle"></div><!-- /.c-circle -->
         </div><!-- /.p-link -->
-        <div class="p-link">
-          <a href="" class="c-link c-link--fill">View more</a><!-- /.c-link -->
-          <div class="c-circle c-circle--fill"></div><!-- /.c-circle -->
-        </div><!-- /.p-link -->
       </div><!-- /.p-sectionHead -->
+      <div class="p-contents fr-about__contents">
+        <div class="fr-about__body">
+          <p class="fr-about__text">急速に広がったグローバル社会に対応できる人材を育成することで、文化・言語の垣根を越えたコミュニケーションを活発にし、一人でも多くの人が豊かに暮らせる世界を実現することを使命とする。</p><!-- /.fr-about__text -->
+          <p class="fr-about__text">コミュニケーションスキル習得をサポートすることで一人でも多くのビジネスパーソンの視野を広げ、世界を舞台に新しい相乗効果を生む未来を創造する。</p><!-- /.fr-about__text -->
+          <p class="fr-about__text">文化の垣根を越えた人と人とのつながりが新しい価値を生むことを信念とする。</p><!-- /.fr-about__text -->
+        </div><!-- /.fr-about__body -->
+        <figure class="fr-about__img">
+          <img src="<?php echo get_template_directory_uri(); ?>/assets/images/front/picture-about.webp" alt="写真：会議中のメンバー" width="681" height="426" loading="lazy" decoding="async">
+        </figure><!-- /.fr-about__img -->
+        <div class="p-link fr-about__link u-hide--flex">
+          <a href="<?php echo esc_url(home_url('/about/')); ?>" class="c-link">View more</a><!-- /.c-link -->
+          <div class="c-circle"></div><!-- /.c-circle -->
+        </div><!-- /.p-link -->
+      </div><!-- /.p-contents -->
     </div><!-- /.l-inner -->
   </section>
 </main>
